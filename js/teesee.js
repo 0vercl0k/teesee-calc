e4f44b9a670caacdc631b0f5ae8dffee2948e86f
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2021-03-18 23:03:48 -0700

    Oops, account for pathname in URL.

--- js/teesee.js
+++ js/teesee.js
@@ -512,7 +512,7 @@ class TeeseeApp_t {
     // Update the URL.
     //
 
-    const NewUrl = `${window.origin}?${Url.join('&')}`;
+    const NewUrl = `${window.origin}${window.pathname}?${Url.join('&')}`;
     window.history.replaceState({}, document.title, NewUrl);
     document.getElementById('url').value = NewUrl;
   }
